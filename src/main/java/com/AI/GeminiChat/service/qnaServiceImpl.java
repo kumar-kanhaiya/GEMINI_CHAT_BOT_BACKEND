1bb0b2460881785da17af45f85fec80304c7e204
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-31 15:46:40 +0530

    code

--- src/main/java/com/AI/GeminiChat/service/qnaServiceImpl.java
+++ src/main/java/com/AI/GeminiChat/service/qnaServiceImpl.java
@@ -1,9 +1,16 @@
 package com.AI.GeminiChat.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class qnaServiceImpl implements qnaService{
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
 
     @Override
     public String getAnswer(String question) {
