a007952df114b365d2ef9fef5ba07ca23d162b0f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-31 16:08:17 +0530

    code

--- src/main/java/com/AI/GeminiChat/service/qnaServiceImpl.java
+++ src/main/java/com/AI/GeminiChat/service/qnaServiceImpl.java
@@ -2,6 +2,9 @@ package com.AI.GeminiChat.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class qnaServiceImpl implements qnaService{
@@ -12,8 +15,45 @@ public class qnaServiceImpl implements qnaService{
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
+    private final WebClient webClient;
+
+    public qnaServiceImpl(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
     @Override
     public String getAnswer(String question) {
-        return "";
+
+        Map<String, Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",question)
+                        })
+
+                }
+        );
+
+//                {
+//    "contents": [
+//      {
+//        "parts": [
+//          {
+//            "text": "Explain how AI works in a few words"
+//          }
+//        ]
+//      }
+//    ]
+//  }
+        // Make API call
+        String responce =
+                webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return responce;
     }
 }
