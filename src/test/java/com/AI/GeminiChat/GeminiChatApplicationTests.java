a771991542a1d84e3fe25a8d163b1e311e94a55c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-31 14:18:25 +0530

    code

---
+++ src/test/java/com/AI/GeminiChat/GeminiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.GeminiChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
